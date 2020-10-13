6ab4723285d57da5a37d21b3f603546d3dda9da8
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2020-10-13 15:15:00 +0100

    Updated dependencies and fixed deprecated config/imports

--- test/mocks/MockHttp.scala
+++ test/mocks/MockHttp.scala
@@ -21,8 +21,7 @@ import org.mockito.Mockito._
 import org.mockito.stubbing.OngoingStubbing
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.mockito.MockitoSugar
-import uk.gov.hmrc.http.HttpResponse
-import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.http.HttpClient
 import uk.gov.hmrc.play.test.UnitSpec
 
 import scala.concurrent.Future
@@ -36,16 +35,8 @@ trait MockHttp extends UnitSpec with MockitoSugar with BeforeAndAfterEach {
     reset(mockHttpGet)
   }
 
-  def setupMockHttpGet[A](url: String)(response: A): OngoingStubbing[Future[A]] =
-    when(mockHttpGet.GET[A](ArgumentMatchers.eq(url))(ArgumentMatchers.any(), ArgumentMatchers.any(),
-      ArgumentMatchers.any())).thenReturn(Future.successful(response))
-
   def setupMockHttpGet[A](url: String, queryParams: Seq[(String, String)])(response: A): OngoingStubbing[Future[A]] =
     when(mockHttpGet.GET[A](ArgumentMatchers.eq(url),
       ArgumentMatchers.eq(queryParams))(ArgumentMatchers.any(), ArgumentMatchers.any(),
       ArgumentMatchers.any())).thenReturn(Future.successful(response))
-
-  def setupMockFailedHttpGet[A](url: String)(response: HttpResponse): OngoingStubbing[Future[A]] =
-    when(mockHttpGet.GET[A](ArgumentMatchers.eq(url))(ArgumentMatchers.any(),
-      ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(Future.failed(new Exception))
 }
